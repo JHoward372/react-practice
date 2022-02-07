d887b70da9c6d50d6ac0b37b4127236cfe085f6f
JHoward372 <jhoward@icstars.org>
2022-02-07 02:31:38 -0600

    Strip down index.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element1 = <h1>Samurai Champloo is real deal one of the rawest animes out. No debate</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element1, document.getElementById('root'));
